8d1db06d828b8ce2c7b2fcb406f28db4815916c6
damienbod <damien_bod@hotmail.com>
2014-08-29 10:27:59 +0200

    clean up

--- PolicemanConsole/TestClass.cs
+++ PolicemanConsole/TestClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Policeman;
 
 namespace PolicemanConsole
 {
@@ -8,8 +9,28 @@ namespace PolicemanConsole
         {
             Policeman.Guard.Requires<ArgumentNullException>("notNull fdfsdfsd", notNull != null);
             //Policeman.Guard.Requires<ArgumentOutOfRangeException>(numberLargerThan5 > 5);
-            Policeman.Guard.ArgumentGreaterOrEqualThan(DateTime.Now, DateTime.Now.AddHours(3.0), "ddd");
-            Policeman.Guard.ArgumentNotNull(null, "TestANullArg");
+            Guard.ArgumentGreaterOrEqualThan(DateTime.Now, DateTime.Now.AddHours(3.0), "ddd");
+            Guard.ArgumentNotNull(null, "TestANullArg");
         }
+
+		private string HelloWorld(int importantParamWhichNeedsaGuard)
+		{
+			// throws a PolicemanException exception if incorrect
+			Guard.Requires<PolicemanException>(
+			 string.Format("Expecting Exception of type PolicemanException, arg= {0}", "importantParamWhichNeedsaGuard"),
+			 importantParamWhichNeedsaGuard == 7
+		   );
+
+			return "Your may continue your logic...";
+		}
+
+		private string HelloMinMax(int min, int max)
+		{
+			Guard.ArgumentGreaterOrEqualThan(min, 3, "min");
+			Guard.ArgumentLowerOrEqualThan(max, 4, "max");
+			
+			// Do you business logic
+			return "Your may continue your logic...";
+		}
     }
 }
